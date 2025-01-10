b8ee3a9a0bd050a805af0218ecbdc65594547861
Dimas <63927493+Yarigore@users.noreply.github.com>
2025-01-10 17:18:34 +0100

    First commit

---
+++ src/test/java/com/visionariosUnidos/ERP/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionariosUnidos.ERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
